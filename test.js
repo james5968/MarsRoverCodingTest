ced926119918ce4aef656c8701888b07b3f1af74
James Ashford <james.ashford@hotmail.com>
2020-10-08 11:26:18 +0100

    Improved test formating

--- test.js
+++ test.js
@@ -1,18 +1,20 @@
 import { moveRover } from './script.js';
 
-let testResults = [];
-
-const testCase = (input, expected) => {
-	if (input === expected) {
-		testResults.push('PASS');
-	} else {
-		testResults.push('FAIL');
-	}
+const testCase = (desc, input, expected) => {
+	let pass = input === expected ? '%cPASS' : '%cFAIL';
+	let css = input === expected ? 'color:green' : 'color:red';
+	console.log(
+		`%cTest case:%c\n${desc}\n%cResult: ${pass} `,
+		'font-size:16px',
+		'color:rgb(200,200,200); font-size:12px',
+		'font-size:14px',
+		`font-size:14px;${css}`
+	);
 };
 
 const test = () => {
-	testResults = [];
 	testCase(
+		'Move rover from initial position 1 2 N with instruction set LMLMLMLMM expected output 1 3 N',
 		moveRover('1 2 N', 'LMLMLMLMM', [
 			5,
 			5
@@ -20,6 +22,7 @@ const test = () => {
 		'1 3 N'
 	);
 	testCase(
+		'Move rover initial position 3 3 E with instruction set MMRMMRMRRM expected out put 5 1 E',
 		moveRover('3 3 E', 'MMRMMRMRRM', [
 			5,
 			5
@@ -27,14 +30,13 @@ const test = () => {
 		'5 1 E'
 	);
 	testCase(
+		'Move rover initial position 1 2 N with instruction set LMLMLMRMM expect the rover to go out of bounds',
 		moveRover('1 2 N', 'LMLMLMRMM', [
 			5,
 			5
 		]),
 		'The rover left the plateau! (Thats going to be an expensive mistake)'
 	);
-
-	console.log(`Test results = ${testResults}`);
 };
 
 test();
